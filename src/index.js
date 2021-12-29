9d59615e2ba6746f6c62d4e00989a4e3eea5dcd8
hossein <hossein.seifi.98@gmail.com>
2021-12-29 19:19:05 +0330

    createElement moved to other file

--- src/index.js
+++ src/index.js
@@ -1,40 +1,5 @@
-function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map((child) =>
-        typeof child === "object" ? child : createTextNode(child)
-      ),
-    },
-  };
-}
-
-function createTextElement(text) {
-  return {
-    type: "TEXT_ELEMENT",
-    props: {
-      nodeValue: text,
-      children: [],
-    },
-  };
-}
-
+/** @jsx  createElement */
+import { createElement } from "./Didact";
 const container = document.getElementById("root");
-const element = {
-  type: "h1",
-  props: {
-    title: "foo",
-    children: "salam",
-  },
-};
 
-const node = document.createElement(element.type);
-node["title"] = element.props.title;
-
-const text = document.createTextNode("");
-text["nodeValue"] = element.props.children;
-
-node.appendChild(text);
-
-container.appendChild(node);
+const element = <h1>hi</h1>;
