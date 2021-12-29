2dff3cd20e33c580075dbd2a708f01ba367c4291
hossein <hossein.seifi.98@gmail.com>
2021-12-29 18:35:06 +0330

    createElement and createTextElement implimented

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-    const container = document.getElementById("root")
-    const element = {
-        type:"h1",
-        props:{
-            title:"foo",
-            children:"salam"
-        }
-    }
-    
-    const node = document.createElement(element.type)
-    node['title'] = element.props.title
-    
-    const text = document.createTextNode('')
-    text['nodeValue'] = element.props.children
-    
-    node.appendChild(text)
-    
-    container.appendChild(node)
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextNode(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const container = document.getElementById("root");
+const element = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: "salam",
+  },
+};
+
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+
+node.appendChild(text);
+
+container.appendChild(node);
